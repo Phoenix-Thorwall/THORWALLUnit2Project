8b49a9354f61f30341bd8bb6c077a3771d687afc
Phoenix Thorwall <phoenixt3@nycstudents.net>
2022-10-17 12:31:50 -0400

    Day One of Work!

---
+++ src/LinearEquation.java
@@ -0,0 +1,17 @@
+public class LinearEquation
+{
+    //Initializing Variables
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+
+    //Creating the Object for y=mx+b
+    public LinearEquation(int x1, int x2, int y1, int y2)
+    {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+}
---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class LinearEquationRunner
+{
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter coordinate 1: ");
+        String coord1 = scan.nextLine();
+        System.out.print("\nEnter coordinate 2: ");
+        String coord2 = scan.nextLine();
+
+        int x1 = Integer.parseInt(coord1.substring(coord1.indexOf("(") + 1, coord1.indexOf(", ")));
+        int y1 = Integer.parseInt(coord1.substring(coord1.indexOf(", ") + 2, coord1.indexOf(")")));
+        int x2 = Integer.parseInt(coord2.substring(coord2.indexOf("(") + 1, coord2.indexOf(", ")));
+        int y2 = Integer.parseInt(coord2.substring(coord2.indexOf(", ") + 2, coord2.indexOf(")")));
+
+        LinearEquation LE = new LinearEquation(x1, x2, y1, y2);
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("The two points are: (" + x1 + ", " + y1 + ")" + "and (" + x2 + ", " + y2 + ")");
+
+    }
+}
